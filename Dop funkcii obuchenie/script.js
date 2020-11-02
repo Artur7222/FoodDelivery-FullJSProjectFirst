5e17c5d91b4131c377a42043520898666f12a449
Ushakov Artur <ushakov.artur93@gmail.com>
2020-11-02 15:45:22 +0500

    Функции конструкторы

--- Dop funkcii obuchenie/script.js
+++ Dop funkcii obuchenie/script.js
@@ -1,17 +1,23 @@
-const now = new Date(); 
+function User (name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`)
+    };
+}
 
-// // console.log(now.getHours());
-// // console.log(now.getUTCHours());
+User.prototype.exit = function(name) {
+    console.log(`Пользователь ${this.name} ушел`);
+}
 
-// // console.log(now.getTimezoneOffset());
-// console.log(now.getTime());
+const Artur = new User ('Artur', 28); 
+const Dasha = new User ('Dasha', 18);
 
-let start = new Date();
+Artur.exit(); 
 
-for (let i = 0; i < 100000; i++) {
-    let some = i ** 3;
-};
+Artur.hello(); 
+Dasha.hello(); 
 
-let end = new Date(); 
-
-alert(`Цикл отработал за ${end - start} миллисекунд`);
+console.log(Artur);
+console.log(Dasha); 
